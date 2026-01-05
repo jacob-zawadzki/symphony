1b245481cd723003cca4458f6222fbb309f3ec02
Amit <theamit.969@gmail.com>
2026-01-06 03:30:37 +0530

    fix: screensaver centering, shell token error, notification order, waybar autostart

--- install/themes/hooks/10-waybar.sh
+++ install/themes/hooks/10-waybar.sh
@@ -4,8 +4,7 @@ src="$CURRENT_LINK/.config/waybar/colors.css"
 [[ -f "$src" ]] || exit 0
 ln -sf "$src" "$HOME/.config/waybar/colors.css"
 
-if pgrep -x waybar &>/dev/null; then
-	pkill -x waybar
-	setsid uwsm-app -- waybar >/dev/null 2>&1 &
-fi
+pkill -x waybar 2>/dev/null
+sleep 0.3
+setsid uwsm-app -- waybar >/dev/null 2>&1 &
 exit 0
